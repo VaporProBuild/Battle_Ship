6d5b5e06d548a17e9ad839bad6fb9b6efaf67e05
Bradley Schaefer <97250667+bradleySchaeferComputerEng@users.noreply.github.com>
2023-09-05 18:44:15 +0000

    Linted

--- api.py
+++ api.py
@@ -1,5 +1,6 @@
-#api.py
+# api.py
 import copy
+
 """
 API for the classic battleship game. The first block of code is meant for the 
 declearation of global constantants that are used in the main testing and or
@@ -10,31 +11,50 @@ the board is defined as list[row][col]
 Please refer to the README.md file for instructions on how to run the program.
 
 """
-alpha_to_int = {    #dict to help convert intput to check
-	'A' : 0,
-	'B' : 1,
-	'C' : 2,
-	'D' : 3,
-	'E' : 4,
-	'F' : 5,
-	'G' : 6,
-	'H' : 7,
-	'I' : 8,
-	'J' : 9,
-	'K' : 10,
-	'L' : 11
+alpha_to_int = {  # dict to help convert intput to check
+    "A": 0,
+    "B": 1,
+    "C": 2,
+    "D": 3,
+    "E": 4,
+    "F": 5,
+    "G": 6,
+    "H": 7,
+    "I": 8,
+    "J": 9,
+    "K": 10,
+    "L": 11,
 }
 
-boat_names = {
-    1 : "Suiciders",
-    2 : "Destroyer",
-    3 : "Cruiser",
-    4 : "Battleship",
-    5 : "Carrier",
-    6 : "Helicarrier"
-}
+boat_names = {1: "Suiciders", 2: "Destroyer", 3: "Cruiser", 4: "Battleship", 5: "Carrier", 6: "Helicarrier"}
 
-alpha = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','X','Y','Z']
+alpha = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "X",
+    "Y",
+    "Z",
+]
 
 size_of_board = 15
 
@@ -46,11 +66,13 @@ destroyer = 2
 
 all_ships = [carrier, battleship, cruiser, submarine, destroyer]
 
+
 def initialize_battlefield(size_of_board):
-    '''
+    """
     initializes the board depending on the size given.
-    '''
+    """
     return [[0 for _ in range(size_of_board)] for _ in range(size_of_board)]
+
 
 def xrow_and_col(arr, ships_left):
     total = 0
@@ -60,84 +82,87 @@ def xrow_and_col(arr, ships_left):
         for var in range(len(arr)):
             for index1 in range(len(arr)):
                 index2 = index1 + ship
-                if index2>len(arr):
+                if index2 > len(arr):
                     break
                 col_fits = True
                 row_fits = True
-                for j in range (index1,index2):
-                    if arr[var][j] == 'X':
+                for j in range(index1, index2):
+                    if arr[var][j] == "X":
                         col_fits = False
                         break
                 if col_fits:
-                    for j in range(index1,index2):
+                    for j in range(index1, index2):
                         arr[var][j] += 1
                         total += 1
-                for j in range (index1,index2):
-                    if arr[j][var] == 'X':
+                for j in range(index1, index2):
+                    if arr[j][var] == "X":
                         row_fits = False
                         break
                 if row_fits:
-                    for j in range(index1,index2):
+                    for j in range(index1, index2):
                         arr[j][var] += 1
                         total += 1
     return total
+
 
 def row_and_col(arr, ship) -> int:
     total = 0
     for col in range(len(arr)):
         for index1 in range(len(arr)):
             index2 = index1 + ship
-            if index2>len(arr):
+            if index2 > len(arr):
                 break
             col_fits = True
             row_fits = True
-            for j in range (index1,index2):
-                if arr[col][j] == 'M':
+            for j in range(index1, index2):
+                if arr[col][j] == "M":
                     col_fits = False
-            if col_fits == True and arr[col][j] != 'H':
-                for j in range(index1,index2):
-                    if arr[col][j] != 'H':
+            if col_fits == True and arr[col][j] != "H":
+                for j in range(index1, index2):
+                    if arr[col][j] != "H":
                         arr[col][j] += 1
                         total += 1
-            for j in range (index1,index2):
-                if arr[j][col] == 'M':
+            for j in range(index1, index2):
+                if arr[j][col] == "M":
                     row_fits = False
-            if row_fits and arr[j][col] != 'H':
-                for j in range(index1,index2):
-                    if arr[j][col] != 'H':
+            if row_fits and arr[j][col] != "H":
+                for j in range(index1, index2):
+                    if arr[j][col] != "H":
                         arr[j][col] += 1
                         total += 1
     return total
 
-def print_battlefield(arr):
 
+def print_battlefield(arr):
     for i in range(len(arr)):
         if i == 0:
-            print(f'    {alpha[i]}', end=' ')    
+            print(f"    {alpha[i]}", end=" ")
         else:
-            print(f' {alpha[i]}', end=' ')
+            print(f" {alpha[i]}", end=" ")
     print()
 
     for i in range(len(arr)):
         if i >= 9:
-            print(f'{i+1}|', end='')
+            print(f"{i+1}|", end="")
         else:
-            print(f'{i+1} |', end='')
+            print(f"{i+1} |", end="")
         for j in range(len(arr[i])):
-            if j == len(arr[i])-1:  #print the board
-                print(arr[i][j], end='')
+            if j == len(arr[i]) - 1:  # print the board
+                print(arr[i][j], end="")
             else:
-                print(arr[i][j], end=' ')
+                print(arr[i][j], end=" ")
         print("|")
     print()
+
 
 def emptyBoard(arr, ships_left):
     for i in range(len(arr)):
         for j in range(len(arr[i])):
-            if arr[i][j] not in ['H', 'M', 'X', 'x']:
+            if arr[i][j] not in ["H", "M", "X", "x"]:
                 arr[i][j] = 0
-    
+
     xrow_and_col(arr, ships_left)
+
 
 def update_board(arr: list, row: int, col: int, ships: list) -> None:
     if row < 0 or col < 0 or row > len(arr) or arr > len(arr) or not ships:
@@ -150,18 +175,18 @@ def input_board(col, row, felid) -> bool:
         print("1.error invalid input!")
 
     col = alpha_to_int[(col).upper()]
-    row = int(row)-1
+    row = int(row) - 1
 
     if col > size_of_board or row > size_of_board:
-        print("2.error invalid input!", col, row,size_of_board)
+        print("2.error invalid input!", col, row, size_of_board)
 
-    felid[row][col] = 'X'
+    felid[row][col] = "X"
+
 
 def main():
+    felid = initialize_battlefield(size_of_board)  # initilize feild with boarder of 3
 
-    felid = initialize_battlefield(size_of_board)    #initilize feild with boarder of 3
-
-    total = 1 #in order to get while loop to begin
+    total = 1  # in order to get while loop to begin
 
     while total > 0:
         total = xrow_and_col(felid, all_ships)
@@ -172,15 +197,14 @@ def main():
 
         emptyBoard(felid, all_ships)
 
-        #update_board(felid, row, col, all_ships)
-            #print_battlefield_percentages(felid, total)
+        # update_board(felid, row, col, all_ships)
+        # print_battlefield_percentages(felid, total)
 
-            
-            #print(felid)
-
-
-#DO FUNCTION FOR WHEN BATTLESHIP GETS HIT NEXT!!!!!
+        # print(felid)
 
 
-if __name__ == '__main__':
+# DO FUNCTION FOR WHEN BATTLESHIP GETS HIT NEXT!!!!!
+
+
+if __name__ == "__main__":
     main()
--- frontend.py
+++ frontend.py
@@ -1,173 +1,209 @@
 import tkinter as tk
-import tkinter.messagebox as messagebox
 import copy
 from api import size_of_board as sob
 from api import alpha, boat_names
 from api import all_ships as ships
 from api import initialize_battlefield, xrow_and_col, emptyBoard
 
+
 class TableButton(tk.Button):
+    # Used to iterate through the buttons easily
+    button_list = []
 
-	#Used to iterate through the buttons easily
-	button_list = []
+    def __init__(self, master, row, col, *args, **kwargs):
+        super().__init__(master, *args, **kwargs)
+        self.row = row
+        self.col = col
+        self.button_list.append(self)
 
-	def __init__(self, master, row, col, *args, **kwargs):
-		super().__init__(master, *args, **kwargs)
-		self.row = row
-		self.col = col
-		self.button_list.append(self)
+    def set_text(self, text):
+        self.configure(text=text)
 
-	def set_text(self, text):
-		self.configure(text=text)
+    # Updates the text (and colour in the future of the buttons
+    def update_all_texts(self, text, arr):
+        row = 0
+        col = 0
+        for btn in self.button_list:
+            btn.configure(text=f"{arr[row][col]}")
+            col += 1
+            if col == sob:
+                col = 0
+                row += 1
 
-	#Updates the text (and colour in the future of the buttons
-	def update_all_texts(self, text, arr):
-		row = 0
-		col = 0
-		for btn in self.button_list:
-			btn.configure(text=f"{arr[row][col]}")
-			col += 1
-			if col == sob:
-				col = 0
-				row += 1
 
-#All the buttons call this function
+# All the buttons call this function
 def button_callback(button, label_text, arr, copies):
-	if arr[button.row][button.col] == 'X':	#already accounted for
-		return
-	copies.append(copy.deepcopy(arr))	#create a copy of the old map
-	arr[button.row][button.col] = 'X'
-	label_text.set(f"Button at row {button.row}, column {button.col} was clicked")
-	button.set_text(f"{arr[button.row][button.col]}")
-	emptyBoard(arr, ships)
-	button.update_all_texts(" ", arr)
-	for all in copies:
-		print(all)
-		print()
+    if arr[button.row][button.col] == "X":  # already accounted for
+        return
+    copies.append(copy.deepcopy(arr))  # create a copy of the old map
+    arr[button.row][button.col] = "X"
+    label_text.set(f"Button at row {button.row}, column {button.col} was clicked")
+    button.set_text(f"{arr[button.row][button.col]}")
+    emptyBoard(arr, ships)
+    button.update_all_texts(" ", arr)
+    for all in copies:
+        print(all)
+        print()
+
 
 def str_ships_sidebar(ships):
-	if len(ships) == 0:
-		return "------------------------\n!WOOT WOOT U WIN!\n------------------------"
-	lst = "List of ships Remaining\n------------------------\n"
-	for ship in ships:
-		lst += (f"{boat_names[ship]} ({ship} cells)\n")
-	return lst
+    if len(ships) == 0:
+        return "------------------------\n!WOOT WOOT U WIN!\n------------------------"
+    lst = "List of ships Remaining\n------------------------\n"
+    for ship in ships:
+        lst += f"{boat_names[ship]} ({ship} cells)\n"
+    return lst
 
-#Inital call that creates and itilizes everything
+
+# Inital call that creates and itilizes everything
 def create_table(root, sob, arr, copies):
-	label_text = tk.StringVar()
-	label_text.set("Click a button to see its row and column index")
-	
-	for row in range(sob+1):
-		for col in range(sob+1):
-			if row == 0  and col == 0:
-				label = tk.Label(root, text="0")
-				label.grid(row=row, column=col)
-			elif col == 0:
-				label = tk.Label(root, text=f"{row}")
-				label.grid(row=row, column=col)
-			elif row == 0:
-				label = tk.Label(root, text=f"{alpha[col]}")
-				label.grid(row=row, column=col)
-			else:
-				button = TableButton(root, row-1, col-1, text=f"{arr[row-1][col-1]}")
-				button.config(command=lambda button=button, label_text=label_text, arr=arr, copies=copies: button_callback(button, label_text, arr, copies), width=3, height=3)
-				button.grid(row=row, column=col)
-	
-	# Update the frame to get its current size
-	root.update()
+    label_text = tk.StringVar()
+    label_text.set("Click a button to see its row and column index")
 
-	#list_frame = tk.Frame(root, width=400, height=len(ships) * 20)
-	#list_frame.grid(row=1, column=sob+1, rowspan=sob+1, sticky='n')
-	label_list_ships = tk.Label(root, text=str_ships_sidebar(ships))
-	label_list_ships.grid(row=1, column=sob+1, rowspan=sob+1, sticky='n')
+    for row in range(sob + 1):
+        for col in range(sob + 1):
+            if row == 0 and col == 0:
+                label = tk.Label(root, text="0")
+                label.grid(row=row, column=col)
+            elif col == 0:
+                label = tk.Label(root, text=f"{row}")
+                label.grid(row=row, column=col)
+            elif row == 0:
+                label = tk.Label(root, text=f"{alpha[col]}")
+                label.grid(row=row, column=col)
+            else:
+                button = TableButton(root, row - 1, col - 1, text=f"{arr[row-1][col-1]}")
+                button.config(
+                    command=lambda button=button, label_text=label_text, arr=arr, copies=copies: button_callback(
+                        button, label_text, arr, copies
+                    ),
+                    width=3,
+                    height=3,
+                )
+                button.grid(row=row, column=col)
 
-#	for ship in ships:
-#		shp_label = tk.Label(list_frame, text=f"{boat_names[ship]} ({ship} cells)", width=15, height=1)
-	#	shp_label.pack()
-	root.update()
-	
-	label = tk.Label(root, textvariable=label_text, width = root.winfo_width(), height = 2, anchor="center", justify="center")
-	label.place(relx=0.5, rely=1.0, anchor="s", y=-55)
+    # Update the frame to get its current size
+    root.update()
 
-	btn_frame = tk.Frame(root, width=200, height = 2)
-	btn_frame.place(relx=0.5, rely=1.0, anchor="s", y=-35)
+    # list_frame = tk.Frame(root, width=400, height=len(ships) * 20)
+    # list_frame.grid(row=1, column=sob+1, rowspan=sob+1, sticky='n')
+    label_list_ships = tk.Label(root, text=str_ships_sidebar(ships))
+    label_list_ships.grid(row=1, column=sob + 1, rowspan=sob + 1, sticky="n")
 
-	hit_btn = tk.Button(btn_frame, text="HIT!", width = 10, height = 1, anchor="center", justify="center")
-	hit_btn.pack(side=tk.LEFT, padx=10)
+    # 	for ship in ships:
+    # 		shp_label = tk.Label(list_frame, text=f"{boat_names[ship]} ({ship} cells)", width=15, height=1)
+    # 	shp_label.pack()
+    root.update()
 
-	sunk_btn = tk.Button(btn_frame, command=lambda:sunk_ship(root, ships, arr, label_list_ships), text="SUNK!", width = 10, height = 1, anchor="center", justify="center")
-	sunk_btn.pack(side=tk.LEFT, padx=10)
+    label = tk.Label(
+        root, textvariable=label_text, width=root.winfo_width(), height=2, anchor="center", justify="center"
+    )
+    label.place(relx=0.5, rely=1.0, anchor="s", y=-55)
 
-	undo_btn = tk.Button(root, command=lambda:undo(root, arr, copies), text="UNDO", width = 10, height = 1, anchor="center", justify="center")
-	undo_btn.place(relx=0.5, rely=1.0, anchor="s", y=-5)
-	#print(f"{root.winfo_reqwidth()} + {root.winfo_reqwidth()}")
-	root.geometry(f"{root.winfo_reqwidth()}x{root.winfo_reqheight() + 100}")
+    btn_frame = tk.Frame(root, width=200, height=2)
+    btn_frame.place(relx=0.5, rely=1.0, anchor="s", y=-35)
 
-#When the user presses undo the last version of the board will be inputed into frame
+    hit_btn = tk.Button(btn_frame, text="HIT!", width=10, height=1, anchor="center", justify="center")
+    hit_btn.pack(side=tk.LEFT, padx=10)
+
+    sunk_btn = tk.Button(
+        btn_frame,
+        command=lambda: sunk_ship(root, ships, arr, label_list_ships),
+        text="SUNK!",
+        width=10,
+        height=1,
+        anchor="center",
+        justify="center",
+    )
+    sunk_btn.pack(side=tk.LEFT, padx=10)
+
+    undo_btn = tk.Button(
+        root,
+        command=lambda: undo(root, arr, copies),
+        text="UNDO",
+        width=10,
+        height=1,
+        anchor="center",
+        justify="center",
+    )
+    undo_btn.place(relx=0.5, rely=1.0, anchor="s", y=-5)
+    # print(f"{root.winfo_reqwidth()} + {root.winfo_reqwidth()}")
+    root.geometry(f"{root.winfo_reqwidth()}x{root.winfo_reqheight() + 100}")
+
+
+# When the user presses undo the last version of the board will be inputed into frame
 def undo(master, arr, copies):
-	print("TEst", len(copies))
+    print("TEst", len(copies))
 
-	if len(copies) == 0 or copies == []:
-		print("you are at the beginning!!")
-		return
-	
-	#if user hasnt changed since last time only pop once
-	#print(copies)
-	prev = copies.pop().copy()
-	for i in range(len(arr)):
-		for j in range(len(arr[i])):
-			arr[i][j] = prev[i][j]
+    if len(copies) == 0 or copies == []:
+        print("you are at the beginning!!")
+        return
 
-	tb = TableButton(master, 0, 0)
-	tb.button_list.pop()
-	tb.update_all_texts("", arr)
+    # if user hasnt changed since last time only pop once
+    # print(copies)
+    prev = copies.pop().copy()
+    for i in range(len(arr)):
+        for j in range(len(arr[i])):
+            arr[i][j] = prev[i][j]
 
-#When the user shinks a ship they press this button
+    tb = TableButton(master, 0, 0)
+    tb.button_list.pop()
+    tb.update_all_texts("", arr)
+
+
+# When the user shinks a ship they press this button
 def sunk_ship(master, ships_remaining, arr, label_list_ships):
-	def remove_ship(ship):
-		ships_remaining.remove(ship)
-		emptyBoard(arr, ships_remaining)
-		tb = TableButton(master, 0, 0)
-		tb.button_list.pop()
-		tb.update_all_texts("", arr)
-		label_list_ships.config(text=str_ships_sidebar(ships_remaining))
-		popup.destroy()
+    def remove_ship(ship):
+        ships_remaining.remove(ship)
+        emptyBoard(arr, ships_remaining)
+        tb = TableButton(master, 0, 0)
+        tb.button_list.pop()
+        tb.update_all_texts("", arr)
+        label_list_ships.config(text=str_ships_sidebar(ships_remaining))
+        popup.destroy()
 
-	popup = tk.Toplevel()
-	height = 25 + len(ships_remaining) * 30
-	popup.geometry(f"300x{height}")
-	popup.title("Sunk Ships")
+    popup = tk.Toplevel()
+    height = 25 + len(ships_remaining) * 30
+    popup.geometry(f"300x{height}")
+    popup.title("Sunk Ships")
 
-	label = tk.Label(popup, text="Choose what ship was sunk:", width=20)
-	label.pack()
-	popup.update()
-	for ship in ships_remaining:
-		button = tk.Button(popup, text=f"{boat_names[ship]} ({ship} cells)", command=lambda ship=ship: remove_ship(ship), width=15, height=1)
-		button.pack()
-	popup.update()
-	popup.geometry(f"{popup.winfo_reqwidth()}x{popup.winfo_reqheight()}")
-	popup.mainloop()
+    label = tk.Label(popup, text="Choose what ship was sunk:", width=20)
+    label.pack()
+    popup.update()
+    for ship in ships_remaining:
+        button = tk.Button(
+            popup,
+            text=f"{boat_names[ship]} ({ship} cells)",
+            command=lambda ship=ship: remove_ship(ship),
+            width=15,
+            height=1,
+        )
+        button.pack()
+    popup.update()
+    popup.geometry(f"{popup.winfo_reqwidth()}x{popup.winfo_reqheight()}")
+    popup.mainloop()
 
-#Main of program. Starts the window and code.
+
+# Main of program. Starts the window and code.
 def main():
-	all_battlefields = []
+    all_battlefields = []
 
-	felid = initialize_battlefield(sob)
-	total = xrow_and_col(felid, ships)
-	
-	root = tk.Tk()
-	height = 23 + sob * 62 + 90	#topbar + num_squares * length_squares + Bottom extension
-	width = 23 + sob * 70
-	root.geometry(f"{width}x{height}")
+    felid = initialize_battlefield(sob)
+    total = xrow_and_col(felid, ships)
 
-	create_table(root, sob, felid, all_battlefields)
+    root = tk.Tk()
+    height = 23 + sob * 62 + 90  # topbar + num_squares * length_squares + Bottom extension
+    width = 23 + sob * 70
+    root.geometry(f"{width}x{height}")
 
-	root.mainloop()
+    create_table(root, sob, felid, all_battlefields)
 
-if __name__ == '__main__':
-	main()
+    root.mainloop()
 
-#TODO Add update list functionality
-#TODO Add comments to code
-#TODO Add Hit functionality and colors.
+
+if __name__ == "__main__":
+    main()
+
+# TODO Add update list functionality
+# TODO Add comments to code
+# TODO Add Hit functionality and colors.
